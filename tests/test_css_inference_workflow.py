1b7f685cd86e38c8f58721d9c3d80b65b6407f15
John Koh <me@johnkoh.net>
2024-05-24 23:22:33 +0700

    feat: added Chasm.net testcases

--- tests/test_css_inference_workflow.py
+++ tests/test_css_inference_workflow.py
@@ -5,6 +5,8 @@ import os
 
 import pytest
 
+import json
+
 from infernet_ml.utils.service_models import (
     ConvoMessage,
     CSSCompletionParams,
@@ -20,13 +22,18 @@ from infernet_ml.workflows.inference.css_inference_workflow import CSSInferenceW
         ("OPENAI", "embeddings"),
         ("PERPLEXITYAI", "completions"),
         ("GOOSEAI", "completions"),
+        ("CHASMNET", "prompt"),
+        ("CHASMNET", "workflows"),
     ],
 )
 def test_init(provider: str, endpoint: str) -> None:
     _environ = dict(os.environ)  # or os.environ.copy()
     try:
         os.environ[f"{provider}_API_KEY"] = "test_key"
-        _: CSSInferenceWorkflow = CSSInferenceWorkflow(provider, endpoint)
+        if provider != "CHASMNET":
+            _: CSSInferenceWorkflow = CSSInferenceWorkflow(provider, endpoint)
+        else:
+            _: 
     finally:
         os.environ.clear()
         os.environ.update(_environ)
@@ -57,6 +64,54 @@ def test_completion_inferences(
         )
         req: CSSRequest = CSSRequest(model=model, params=params)
         workflow: CSSInferenceWorkflow = CSSInferenceWorkflow(provider, "completions")
+        workflow.setup()
+        res = workflow.inference(req.model_dump())
+        assert len(res)
+
+
+@pytest.mark.parametrize(
+    "provider, endpoint_id, body",
+    [
+        (
+            "CHASMNET",
+            "111",
+            json.loads('{"input": {"test_input_1": "apple"}}'),
+        ),
+    ],
+)
+def test_chasm_prompt_inferences(
+    provider: str, model: str, messages: list[ConvoMessage]
+) -> None:
+    if os.environ.get(f"{provider}_API_KEY"):
+        params: ChasmPromptParams = ChasmPromptParams(
+            endpoint="prompt", messages=messages
+        )
+        req: ChasmRequest = ChasmRequest(model=model, params=params)
+        workflow: CSSInferenceWorkflow = CSSInferenceWorkflow(provider, "prompt")
+        workflow.setup()
+        res = workflow.inference(req.model_dump())
+        assert len(res)
+
+
+@pytest.mark.parametrize(
+    "provider, endpoint_id, body",
+    [
+        (
+            "CHASMNET",
+            "111",
+            json.loads('{"input": {"test_input_1": "apple"}}'),
+        ),
+    ],
+)
+def test_chasm_workflows_inferences(
+    provider: str, model: str, messages: list[ConvoMessage]
+) -> None:
+    if os.environ.get(f"{provider}_API_KEY"):
+        params: ChasmPromptParams = ChasmPromptParams(
+            endpoint="workflows", messages=messages
+        )
+        req: ChasmRequest = ChasmRequest(model=model, params=params)
+        workflow: CSSInferenceWorkflow = CSSInferenceWorkflow(provider, "workflows")
         workflow.setup()
         res = workflow.inference(req.model_dump())
         assert len(res)
